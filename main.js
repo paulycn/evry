ba57d8f42af366abd3770037d2a019981d2bd843
Paula Cirlan <cirlan.parascovia@gmail.com>
2023-02-23 11:29:03 +0200

    update apikey

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const apiKey = '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'
+const apiKey = 'xsmtpsib-9c1850727afed1e3bc46656a7fcd62bbac9f8c09a84dd40cc55252eabeef12e3-wgt2mE8vMAzCyLs7'
 
 const app = Vue.createApp({
     data() {
